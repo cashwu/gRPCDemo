1e290a500278cd6842effcbd84979ec9b6b97df2
Cash Wu <cash@cashwu.com>
2018-05-10 10:00:05 +0800

    Add project files.

---
+++ gRPCClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+using GRPCDemo;
+
+namespace gRPCClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Channel channel = new Channel("127.0.0.1", 9999, ChannelCredentials.Insecure);
+
+            var client = new gRPC.gRPCClient(channel);
+            var reply = client.SayHello(new HelloRequest { Name = "cash" });
+
+            Console.WriteLine($" reply :: {reply.Message}");
+
+            channel.ShutdownAsync().Wait();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ gRPCServer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+using GRPCDemo;
+
+namespace gRPCServer
+{
+    class gRPCImpl : gRPC.gRPCBase
+    {
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            Console.WriteLine($"request msg :: {request.Name}");
+            return Task.FromResult(new HelloReply { Message = $"Hello {request.Name}" });
+        }
+    }
+
+    class Program
+    {
+        private const int Port = 9999;
+
+        static void Main(string[] args)
+        {
+            Server server = new Server
+            {
+                Services = {gRPC.BindService(new gRPCImpl())},
+                Ports = {new ServerPort("localhost", Port, ServerCredentials.Insecure)}
+            };
+
+            server.Start();
+
+            Console.WriteLine($" grpc server listening on port :: {Port}");
+            Console.ReadKey();
+
+            server.ShutdownAsync().Wait();
+        }
+    }
+}
